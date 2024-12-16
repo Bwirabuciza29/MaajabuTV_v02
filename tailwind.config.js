ddff3ad5ac1cacca8c513a551483a3efd40152fb
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-12-16 09:25:42 +0200

    HereWeAre

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./components/**/*.{js,vue,ts}", "./layouts/**/*.vue", "./pages/**/*.vue", "./plugins/**/*.{js,ts}", "./app.vue", "./error.vue"],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["General Sans", "sans-serif"],
-				serif: ["General Sans", "serif"]
-			}
-		}
-	},
-	plugins: []
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "custom-blue": "#0714D4",
+      },
+      fontFamily: {
+        sans: ["General Sans", "sans-serif"],
+        serif: ["General Sans", "serif"],
+      },
+    },
+  },
+  plugins: [],
 };
